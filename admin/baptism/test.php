5fe89645aca2ae2df4e2e4a4c9d3105d1a73e262
Leonard Topno <leo.topno@gmail.com>
2020-10-25 03:58:28 +0530

    dependent dropdown list table 1

--- admin/baptism/test.php
+++ admin/baptism/test.php
@@ -52,45 +52,7 @@ $(document).on('change','#country', function(){
   </script>
 
 
-
-<?php
-include('connection.php');
-// Fetching state data
-$country_id=!empty($_POST['country_id'])?$_POST['country_id']:'';
-if(!empty($country_id))
-  {
-        $contryData="SELECT id, name from states WHERE country_id=$country_id";
-        $result=mysqli_query($con,$contryData);
-        if(mysqli_num_rows($result)>0)
-        {
-          echo "<option value=''>Select State</option>";
-          while($arr=mysqli_fetch_assoc($result))
-          {
-            echo "<option value='".$arr['id']."'>".$arr['name']."</option><br>";
-        
-          }
-        }  
-   }
-   // Fetching city data
-$state_id=!empty($_POST['state_id'])?$_POST['state_id']:'';
-if(!empty($state_id))
-  {
-        $cityData="SELECT id, name from cities WHERE state_id=$state_id";
-        $result=mysqli_query($con,$cityData);
-        if(mysqli_num_rows($result)>0)
-        {
-          echo "<option value=''>Select City</option>";
-          while($arr=mysqli_fetch_assoc($result))
-          {
-            echo "<option value='".$arr['id']."'>".$arr['name']."</option><br>";
-        
-          }
-        }  
-   }
-   
-         ?>
-
-   <!--===== dependent dropdown form============-->
+ <!--===== dependent dropdown form============-->
 <div class="dependent-dropdown">
 <form autocomplete="off" action="">
   <div class="input-field">
