a2f88d5dfa7611b9930c26fcd13f0e0b2b1212ba
Leonard Topno <leo.topno@gmail.com>
2020-10-25 04:10:40 +0530

    dependent dropdown list table 3

--- admin/baptism/test.php
+++ admin/baptism/test.php
@@ -5,53 +5,8 @@ include('connection.php');
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-
 </head>     
 <body>
-
-<script>
-// ajax script for getting state data
-$(document).on('change','#country', function(){
-      var countryID = $(this).val();
-      if(countryID){
-          $.ajax({
-              type:'POST',
-              url:'backend-script.php',
-              data:{'country_id':countryID},
-              success:function(result){
-                  $('#state').html(result);
-                 
-              }
-          }); 
-      }else{
-          $('#state').html('<option value="">Country</option>');
-          $('#city').html('<option value=""> State </option>'); 
-      }
-  });
-
-    // ajax script for getting  city data
-   $(document).on('change','#state', function(){
-      var stateID = $(this).val();
-      if(stateID){
-          $.ajax({
-              type:'POST',
-              url:'backend-script.php',
-              data:{'state_id':stateID},
-              success:function(result){
-                  $('#city').html(result);
-                 
-              }
-          }); 
-      }else{
-          $('#city').html('<option value="">City</option>');
-          
-      }
-  });
-  </script>
-
-
  <!--===== dependent dropdown form============-->
 <div class="dependent-dropdown">
 <form autocomplete="off" action="">
