490b7ad9d68022442a9502b9b29ec330eabe990a
Leonard Topno <leo.topno@gmail.com>
2020-10-26 02:56:03 +0530

    Authentication of Certificate

--- admin/baptism/print_baptism.php
+++ admin/baptism/print_baptism.php
@@ -173,10 +173,10 @@ $pdf->Cell(60,600, htmlspecialchars($Minister) . '',0,0,L,false);
 //
 $pdf->SetXY(30,110);
 $pdf->SetFont('Arial','',10);
-$pdf->Cell(10,650,'Certified that the above is a true extract from the Register of Baptism kept at .................',0,0,L,false);
+$pdf->Cell(10,750,'Certified that the above is a true extract from the Register of Baptism kept at .................',0,0,L,false);
 
 
-//$pdf->SetXY(430,220);
+//
 $pdf->SetXY(430,700);
 $pdf->Cell(50,10,'Signature & Seal',0,1,L,false);
 
