0236479fd05aaaf1ea278dea4366250e141a5376
Leonard Topno <leo.topno@gmail.com>
2020-10-25 07:40:37 +0530

    removed common section and made it a file to be included

--- admin/baptism/form_baptism copy.php
+++ admin/baptism/form_baptism copy.php
@@ -358,13 +358,93 @@ $id=$_SESSION["username"];
                 <!-- ============================================================== -->
                 <!-- Sales Cards  -->
                 <!-- ============================================================== -->
-                
-                
-                <?php include('bapt-includes/modal.php'); ?>
-                
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Baptism Registers</h4>
+                                <!-- Create the editor container -->
+                                <div class="row">
+                                    <!-- Column -->
+                                    <div class="col-md-6 col-lg-4 col-xlg-3">
+                                        <a href="form_baptism.php">
+                                          <div class="card card-hover">
+                                           
+                                            <div class="box bg-cyan text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
+                                                <h6 class="text-white">Create Baptism Record</h6>
+                                            
+                                            </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <!-- Column -->
+                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
+                                        <a href="../admin/communion/index.php">
+                                           <div class="card card-hover">
+                                            <div class="box bg-success text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-chart-areaspline"></i></h1>
+                                                <h6 class="text-white">1st Communion</h6>
+                                            </div>
+                                        </div>
+										</a>
+                                    </div>-->
+                                     <!-- Column -->
+                                    <div class="col-md-6 col-lg-4 col-xlg-3">
+                                       <a href="edit_baptism.php">
+                                        <div class="card card-hover">
+                                            <div class="box bg-warning text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-collage"></i></h1>
+                                                <h6 class="text-white">Edit Baptism Record</h6>
+                                            </div>
+                                        </div>
+										</a>
+                                    </div>
+                                    <!-- Column -->
+                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
+                                       <a href="../admin/marriage/index.php">
+                                        <div class="card card-hover">
+                                            <div class="box bg-danger text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-border-outside"></i></h1>
+                                                <h6 class="text-white">Marriage</h6>
+                                            </div>
+                                        </div>
+										</a>
+                                    </div>-->
+                                    <!-- Column -->
+                                    <div class="col-md-6 col-lg-4 col-xlg-3">
+                                       <a href="search_baptism.php">
+                                        <div class="card card-hover">
+                                            <div class="box bg-info text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-arrow-all"></i></h1>
+                                                <h6 class="text-white">Search Baptism Record</h6>
+                                            </div>
+                                        </div>
+										</a>
+                                    </div>
+                                    <!-- Column -->
+                                    <!-- Column -->
+                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
+                                       <a href="../admin/search/index.php">
+                                        <div class="card card-hover">
+                                            <div class="box bg-danger text-center">
+                                                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
+                                                <h6 class="text-white">Download /Print</h6>
+                                            </div>
+                                        </div>
+										</a>
+                                    </div>-->
+                                    <!-- Column -->
+                                    
+                                    <!-- Column -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
-
+                
                 <!-- ============================================================== -->
                 <!-- Recent comment and chats -->
                 <div class="row">
