f82f0c97477c8dc9d945a5d0d9cff578dd12ef3d
Leonard Topno <leo.topno@gmail.com>
2020-11-02 04:53:41 +0530

    Included common operations section

--- admin/baptism/index.php
+++ admin/baptism/index.php
@@ -1,18 +1,14 @@
 <?php 
-ob_start(); 
-?>
-<!--  Session Starts   -->
-<?php 
+ob_start();
+#Sessions start
 session_start();
-if (isset($_SESSION["username"]))
-{
+if (isset($_SESSION["username"])){
 	$logid=$_SESSION["username"];
-}
-else
-{
+}else{
 	header("location:../../index.php");
-}
+} 
 ?>
+
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
@@ -176,96 +172,8 @@ else
             <!-- Container fluid  -->
             <!-- ============================================================== -->
             <div class="container-fluid">
-                <!-- ============================================================== -->
-                <!-- Sales Cards  -->
-                <!-- ============================================================== -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">Baptism Registers</h4>
-                                <!-- Create the editor container -->
-                                <div class="row">
-                                    <!-- Column -->
-                                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                                        <a href="form_baptism.php">
-                                          <div class="card card-hover">
-                                           
-                                            <div class="box bg-cyan text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
-                                                <h6 class="text-white">Create Baptism Record</h6>
-                                            
-                                            </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- Column -->
-                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
-                                        <a href="../admin/communion/index.php">
-                                           <div class="card card-hover">
-                                            <div class="box bg-success text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-chart-areaspline"></i></h1>
-                                                <h6 class="text-white">1st Communion</h6>
-                                            </div>
-                                        </div>
-										</a>
-                                    </div>-->
-                                     <!-- Column -->
-                                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                                       <a href="edit_baptism.php">
-                                        <div class="card card-hover">
-                                            <div class="box bg-warning text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-collage"></i></h1>
-                                                <h6 class="text-white">Edit Baptism Record</h6>
-                                            </div>
-                                        </div>
-										</a>
-                                    </div>
-                                    <!-- Column -->
-                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
-                                       <a href="../admin/marriage/index.php">
-                                        <div class="card card-hover">
-                                            <div class="box bg-danger text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-border-outside"></i></h1>
-                                                <h6 class="text-white">Marriage</h6>
-                                            </div>
-                                        </div>
-										</a>
-                                    </div>-->
-                                    <!-- Column -->
-                                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                                       <a href="search_baptism.php">
-                                        <div class="card card-hover">
-                                            <div class="box bg-info text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-arrow-all"></i></h1>
-                                                <h6 class="text-white">Search Baptism Record</h6>
-                                            </div>
-                                        </div>
-										</a>
-                                    </div>
-                                    <!-- Column -->
-                                    <!-- Column -->
-                                    <!--<div class="col-md-6 col-lg-2 col-xlg-3">
-                                       <a href="../admin/search/index.php">
-                                        <div class="card card-hover">
-                                            <div class="box bg-danger text-center">
-                                                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
-                                                <h6 class="text-white">Download /Print</h6>
-                                            </div>
-                                        </div>
-										</a>
-                                    </div>-->
-                                    <!-- Column -->
-                                    
-                                    <!-- Column -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- ============================================================== -->
-                <!-- Recent comment and chats -->
-                <!-- ============================================================== -->
+                <?php include('bapt-includes/modal.php'); ?>
+                <!---------------------------- Dispaying Form   -----------------------------> 
             </div>
             <!-- ============================================================== -->
             <!-- End Container fluid  -->
