fe2b7f5d9a8fae5c9407bd7b67ea4c511f1f9081
Leonard Topno <leo.topno@gmail.com>
2020-11-03 21:02:51 +0530

    Removed obsolate connection

--- admin/baptism/print_baptism.php
+++ admin/baptism/print_baptism.php
@@ -2,12 +2,6 @@
 include('pdf/fpdf.php'); 
 #include('connection.php');
 
-$conn=mysqli_connect("localhost","develope_root","Migids@123","develope_universe");
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
-
 #include('fetch_baptism_details.php');
 
 include('../includes/dbConnect.php');
