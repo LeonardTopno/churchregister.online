c9579a9e1666f90eac6539d85a7fd2cc2a12ac26
Leonard Topno <leo.topno@gmail.com>
2020-11-05 05:08:14 +0530

    Set author name

--- admin/baptism/form_baptism.php
+++ admin/baptism/form_baptism.php
@@ -13,7 +13,7 @@ include('../includes/dbConnect.php');
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Leonard Topno">
     <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
     <title>Baptism Form - Add Record</title>
     <!-- Custom CSS -->
