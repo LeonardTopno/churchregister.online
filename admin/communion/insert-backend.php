278352d7b762d41b56736b854b80f80d5a52415b
Leonard Topno <leo.topno@gmail.com>
2020-11-08 03:35:46 +0530

    insert at backend

--- admin/communion/insert-backend.php
+++ admin/communion/insert-backend.php
@@ -6,10 +6,10 @@ extract($_POST);
 console.log("insert-backend");
 
 
-$first_name=!empty($_POST['firstname'])?$_POST['firstname']:'';
+$first_name=!empty($_POST['first_name'])?$_POST['first_name']:'';
 $last_name="Topno";
 if(!empty($first_name)){
-    $sql = "INSERT INTO userinfo (first_name, last_name) VALUES ('".$first_name."', '".$last_name."')";
+    $sql = "INSERT INTO userinfo (first_name, last_name) VALUES ('$first_name', '$last_name')";
 
     #$sql = "INSERT INTO userinfo (first_name, middle_name,last_name, gender_id, dob, permanent_address, current_address, father_name, father_surname, domicile_id, father_occupation, mother_name, mother_surname, mother_occupation, mobile, email, home_parish_id)
 	#VALUES ('".$FName."','".$MName."','".$LName."','".$Gender."','".$DOB."','".$Padd."','".$Cadd."','".$Fathername."','".$Fathersname."','".$Domicile_ID."','".$Foccupation."','".$Mothername."','".$Mothersname."','".$Moccupation."','".$Mobile."','".$Email."','".$HomeParishId."')";
