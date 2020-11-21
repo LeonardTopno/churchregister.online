eb5ebfec554cbd1f35f3e65bdca3684c6709a951
Leonard Topno <leo.topno@gmail.com>
2020-11-22 03:55:27 +0530

    Just some alignment changes and recap

--- admin/communion/detailcomm.php
+++ admin/communion/detailcomm.php
@@ -645,7 +645,7 @@ include('../includes/dbConnect.php');
                             	<div class="modal-dialog modal-lg">
                                   <div class="modal-content">
                                     <div class="modal-header">
-                                      <h4 class="modal-title">Individual Communion Detail</h4>
+                                      <h4 class="modal-title">1st Communion Detail</h4>
                                       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                                     </div>
                                     <div class="modal-body" id="communion_detail">  
