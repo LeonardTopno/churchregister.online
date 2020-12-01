9351c5b3dd07599c8eddb1848034f74b07850859
Leonard Topno <leo.topno@gmail.com>
2020-12-02 01:16:46 +0530

    1st Communion Detail of Individual

--- admin/communion/detailcomm.php
+++ admin/communion/detailcomm.php
@@ -645,7 +645,7 @@ include('../includes/dbConnect.php');
                             	<div class="modal-dialog modal-lg">
                                   <div class="modal-content">
                                     <div class="modal-header">
-                                      <h4 class="modal-title">1st Communion Detail</h4>
+                                      <h4 class="modal-title">1st Communion Detail of Individual</h4>
                                       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                                     </div>
 
@@ -666,7 +666,7 @@ include('../includes/dbConnect.php');
                                             </div>
                                         </div> 
                                     
-                                    </div>
+                                        <!--</div>-->
                                     
                                    
                                     <div class="modal-footer">
