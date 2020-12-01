7a871669be00be8594af32a7988cb1c91a4cd631
Leonard Topno <leo.topno@gmail.com>
2020-12-02 02:27:24 +0530

    Separated modal in detailcomm.php 1

--- admin/communion/modals/display-user-communion-details.php
+++ admin/communion/modals/display-user-communion-details.php
@@ -3,7 +3,7 @@
    <div class="modal-dialog modal-lg">
       <div class="modal-content">
          <div class="modal-header">
-            <h4 class="modal-title">Individual Communion Detail</h4>
+            <h4 class="modal-title">Individual Communion Detail Leo</h4>
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
          </div>
          <div class="container"></div>
