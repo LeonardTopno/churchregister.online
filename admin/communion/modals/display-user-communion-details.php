1c22bc5e754414d6e01bd2fe69d9075948c07d67
Leonard Topno <leo.topno@gmail.com>
2020-12-05 03:50:54 +0530

    Added all fields in modal for displaying individual Communion Details

--- admin/communion/modals/display-user-communion-details.php
+++ admin/communion/modals/display-user-communion-details.php
@@ -8,24 +8,63 @@
          </div>
          <div class="container"></div>
          <div class="modal-body">
+            
             <div class="form-group row">
                <label class="col-sm-3 text-left control-label col-form-label">Communion ID</label>
                <div class="col-sm-9">
                   <?php echo $id;?>
                </div>
             </div>
+            
             <div class="form-group row">
                <label for="fname" class="col-sm-3 text-left control-label col-form-label">Name</label>
                <div class="col-sm-9">
                   <?php echo $fname;?> &nbsp; <?php echo $MName;?> &nbsp; <?php echo $LName;?>
                </div>
             </div>
+            
+            <div class="form-group row">
+               <label for="fname" class="col-sm-3 text-left control-label col-form-label">Father's Name</label>
+               <div class="col-sm-9">
+                  <?php echo $fname;?> &nbsp; <?php echo $MName;?> &nbsp; <?php echo $LName;?>
+               </div>
+            </div>
+            
+            <div class="form-group row">
+               <label for="fname" class="col-sm-3 text-left control-label col-form-label">Mother's Name</label>
+               <div class="col-sm-9">
+                  <?php echo $fname;?> &nbsp; <?php echo $MName;?> &nbsp; <?php echo $LName;?>
+               </div>
+            </div>
+            
+            <div class="form-group row">
+               <label for="fname" class="col-sm-3 text-left control-label col-form-label">Home Parish</label>
+               <div class="col-sm-9">
+                  <?php echo $fname;?> &nbsp; <?php echo $MName;?> &nbsp; <?php echo $LName;?>
+               </div>
+            </div>
+
+            <div class="form-group row">
+               <label for="fname" class="col-sm-3 text-left control-label col-form-label">Date of Birth</label>
+               <div class="col-sm-9">
+                  <?php echo date("d-m-Y",strtotime($baptims_date));?>
+               </div>
+            </div>
+
+            <div class="form-group row">
+               <label for="fname" class="col-sm-3 text-left control-label col-form-label"> Baptism Date</label>
+               <div class="col-sm-9">
+                  <?php echo date("d-m-Y",strtotime($baptims_date));?>
+               </div>
+            </div>
+
             <div class="form-group row">
                <label for="fname" class="col-sm-3 text-left control-label col-form-label">Communion Date</label>
                <div class="col-sm-9">
                   <?php echo date("d-m-Y",strtotime($baptims_date));?>
                </div>
             </div>
+
             <div class="form-group row">
                <label for="fname" class="col-sm-3 text-left control-label col-form-label">Communion Church</label>
                <div class="col-sm-9">
@@ -39,6 +78,7 @@
                </div>
             </div>
          </div>
+
          <div class="modal-footer">
             <a href="href="print_baptism.php?Id=<?php echo $id; ?>""  class="btn">Print Details</a>
             <a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>
