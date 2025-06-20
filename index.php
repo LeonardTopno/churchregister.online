62a4f538487250d6a109dd8455ba8a526118b31c
Leonard Topno <leo.topno@gmail.com>
2025-06-21 04:39:36 +0530

    Added author to the app

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Church Registers Online">
+    <meta name="author" content="Leonard TOPNO">
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     <title>:: Church Register - Login  ::</title>
