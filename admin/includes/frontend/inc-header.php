723912605df73b9bffb843ff67bf3ca4ebfc5d71
Leonard Topno <leo.topno@gmail.com>
2025-06-25 00:30:15 +0530

    hard coded url made dynamic usinf

--- admin/includes/frontend/inc-header.php
+++ admin/includes/frontend/inc-header.php
@@ -14,14 +14,16 @@
                         <b class="logo-icon p-l-10">
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="../../../assets/images/logo-icon.png" alt="homepage" class="light-logo" />
+                            <!--<img src="../../../assets/images/logo-icon.png" alt="homepage" class="light-logo" />-->
+                            <img src="<?= $base_url ?>assets/images/logo-icon.png" alt="homepage" class="light-logo" />
                            
                         </b>
                         <!--End Logo icon -->
                          <!-- Logo text -->
                         <span class="logo-text">
                              <!-- dark Logo text -->
-                             <img src="../../../assets/images/logo-text.png" alt="homepage" class="light-logo" />
+                             <!--<img src="../../../assets/images/logo-text.png" alt="homepage" class="light-logo" />-->
+                             <img src="<?= $base_url ?>assets/images/logo-text.png" alt="homepage" class="light-logo" />
                             
                         </span>
                         <!-- Logo icon -->
