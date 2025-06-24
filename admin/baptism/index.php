842a81d46e4ab45e890e0ffeba6bf113e1eb3d1e
Leonard Topno <leo.topno@gmail.com>
2025-06-25 00:24:00 +0530

    Updated title tag of baptism index.php

--- admin/baptism/index.php
+++ admin/baptism/index.php
@@ -37,7 +37,7 @@ if ($isLocal) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin_Baptism_Live Church</title>
+    <title>Church Register | Baptism</title>
 
     <link rel="icon" type="image/png" sizes="16x16" href="<?= $base_url ?>assets/images/favicon.png">
     <link href="<?= $base_url ?>assets/libs/flot/css/float-chart.css" rel="stylesheet">
